e78cfd92f487daefc4a84713179262685e51b3c6
donlinglok <donlinglok@192.168.66.104>
2017-03-06 10:40:23 +0800

    add docker-compose install
    Former-commit-id: 4fddaa8d778eaa5aad6da1cab533722a0319ab39

--- install.sh
+++ install.sh
@@ -18,3 +18,7 @@ else
 fi
 
 sudo service docker start
+
+curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > ./docker-compose
+sudo mv ./docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
