92496406ec15bbad99f960feb0a4cbe31c68658c
donlinglok <donlinglok@10.10.12.124>
2017-03-06 10:56:17 +0800

    fix git install
    Former-commit-id: 3f12945812792d53628d5cbce9ca5087386b602a

--- install.sh
+++ install.sh
@@ -3,18 +3,15 @@
 YUM_CMD=$(which yum)
 APT_GET_CMD=$(which apt-get)
 
-if [[ ! -z $YUM_CMD ]]; then
-	sudo yum update -y
-	sudo yum install -y git
-	sudo yum install -y docker
-	sudo usermod -a -G docker ec2-user
-elif [[ ! -z $APT_GET_CMD ]]; then
+if [[ ! -z $APT_GET_CMD ]]; then
 	sudo apt-get update
 	sudo apt-get install -y git
 	curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 else
-    echo "error can't install package $PACKAGE"
-    exit 1;
+	sudo yum update -y
+	sudo yum install -y git
+	sudo yum install -y docker
+	sudo usermod -a -G docker ec2-user
 fi
 
 sudo service docker start
